317112113ae34c4b28352f6146f501a38745c553
vaishali <vaishali1998>
2021-06-11 23:20:58 +0530

    my first page

---
+++ script.js
@@ -0,0 +1,200 @@
+//20may
+
+
+
+//alert('hello')
+//documnet.write('hello')
+//console.log('hello')
+
+
+//typeof
+
+// let firstName='vaishali'
+// console.log(typeof firstName)
+
+
+// let a = firstName.length
+// console.log(typeof a)
+
+
+// let secondname = 'sahi'
+// console.log(typeof secondname)
+
+
+// let d = secondname.length
+// console.log(typeof d)
+
+
+
+
+
+//toUppercase,toLowercase
+
+
+// let firstName = 'sham'
+// console.log(firstName.toLocaleUpperCase())
+// console.log(firstName.toLowerCase())
+
+
+
+//functions
+
+//function declaration
+//function expression
+//arrow function
+
+//1) function declaration
+
+
+//function with parameter with return type
+
+// function add(x,y){
+
+// return x+y
+// }
+// let a = add(5,5)
+// console.log(a)
+
+
+// //function without return type without return value
+
+// function sub(){
+//     console.log(5-4)
+// }
+// sub(6,5)
+
+// // function with parameter without return type
+
+// function mul(t,y){
+// console.log(t*y)
+// }
+// mul(2,3)
+
+
+
+
+// //function without parameter with return type
+
+
+// function piValue(){
+//     return 3.14
+// }
+// let s = piValue(3.14)
+// console.log(s)
+
+
+
+//function expression
+// let abc = function(r,e){
+//     console.log(5+4)
+//     return r+e
+// }
+// let ab = abc(6,5)
+// console.log(ab)
+
+
+//Arrow function
+
+// let abc = (r,e)=>{
+//     console.log(5+4)
+//     return r+e
+// }
+// let ab = abc(6,5)
+// console.log(ab)
+
+
+
+// 21 may
+
+
+//way to write functions
+
+ 
+//  let r = alert('hello')
+//  console.log(r)
+
+
+//  let f = console.log(3+7)
+
+//  console.log(10+10)
+
+
+//  let abc = 'vaishali'
+//  console.log(abc)
+
+//  let d = 'vaishali'
+//  d = 7+9 
+//  console.log(d)
+
+
+//  let surName = 'sAwAnt'
+
+//  console.log(surName.toLowerCase().toUpperCase())
+//  console.log(surName.length)
+//  console.log(typeof surName)
+ 
+
+ // let const var
+
+ //block scoped
+ //1)let
+ //2)const
+
+//  {
+
+//     let y = 20
+//     let d = 30
+//     console.log(y)
+//     console.log(d)
+//  }
+// console.log(d)
+
+
+// {
+
+//     const y = 20
+//     const d = 30
+//     console.log(y)
+//     console.log(d)
+//  }
+// console.log(d)
+
+
+
+// {
+
+//      var y = 20
+//      var d = 30
+//      console.log(y)
+//      console.log(d)
+//   }
+//  console.log(d)
+
+
+//reassign values
+//1)let
+//2)var
+
+
+//  function add(u,i) {
+    
+//  return u+i
+// }
+// let f = add(5,6)
+// console.log(f)
+// console.log(45+67+f)
+// console.log(f+67-78*57%76)
+
+
+
+// var sub = function (q,w) {
+//     return q-w
+    
+// }
+// var d = sub(6,5)
+// console.log(d)
+// console.log(d+10)
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,70 @@
+// let firstName = 'vaishali'
+// console.log(firstName)
+// console.log(typeof firstName)
+
+
+// // 0 1 2 3 4 5 6 7
+// // v a i s h a l i
+
+
+// console.log(firstName[0])
+// console.log(firstName[4])
+
+// //string
+// //method will perform some action
+// //method will return something
+
+
+
+// let upper = firstName.toUpperCase()
+// console.log(upper)
+
+// let lower = firstName.toLocaleLowerCase()
+// console.log(lower)
+
+
+
+
+//indexOf
+
+
+// firstName = 'vaishali'
+
+// let b = firstName.indexOf('h')
+// console.log(b)
+
+
+// let d = firstName.indexOf('i')
+// console.log(d)
+
+// let p = firstName.indexOf('i')
+// console.log(p)
+
+
+// V A i s H A l i
+ 
+
+// let  d = 'VAISHaLI'
+
+// let c = d.indexOf('S', d.indexOf (' ') +1)
+// console.log(c)
+
+
+// let f = d.indexOf('A')
+// console.log(f)
+
+
+// let h = d.indexOf('a')
+// console.log(h)
+
+
+// let j = "apple banana orange papaya tomato apple"
+
+// let v = j.indexOf('apple', j.indexOf('apple'))
+// console.log(v)
+
+let c = "apple manago banana chiku papaya grapes apple "
+ let v = c.indexOf('apple', c.indexOf('apple') +1)
+ console.log(v)
+
+
